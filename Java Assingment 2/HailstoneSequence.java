60755cc8db2b93b5f31c8346c7dfd8d3535c7c3f
mathew pius <mathewpius4232@gmail.com>
2020-08-31 21:54:00 +0530

    ques6 commit

--- Java Assingment 2/HailstoneSequence.java
+++ Java Assingment 2/HailstoneSequence.java
@@ -4,16 +4,25 @@ public class HailstoneSequence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
-        int n = sc.nextInt();
-        System.out.println("Hailstone Sequence of " + n + " is");
-        while (n != 1) {
-            if (n % 2 == 0) {
-                n = n / 2;
+        try {
+            int n = sc.nextInt();
+            if (n <= 0) {
+                throw new ArithmeticException("");
             } else {
-                n = (n * 3) + 1;
+                System.out.println("Hailstone Sequence of " + n + " is");
+                while (n != 1) {
+                    if (n % 2 == 0) {
+                        n = n / 2;
+                    } else {
+                        n = (n * 3) + 1;
+                    }
+                    System.out.println(n);
+                }
             }
-            System.out.println(n);
+        } catch (Exception e) {
+            System.out.println("Value entered is not an Integer or Integer is a negative value or zero");
         }
+
         sc.close();
 
     }
